LineageOS/android_vendor_lineage

vendor/lineage: Add support for java source overlays
*) Set JAVA_SOURCE_OVERLAYS in a device makefile as follows:
   # Java sourcefile overlays (for Android.bp built modules only)
   # Format is a whitespace separated set of rules, each of which
   # structured as follows:
   # modulename|overlay directory|glob pattern within overlay dir
   JAVA_SOURCE_OVERLAYS := \
           org.lineageos.hardware|$(LOCAL_PATH)/lineagehw|**/*.java

*) As of this commit, java sources overlays are only allowed for
   module org.lineageos.hardware.

Change-Id: I6be1c12567081357f5231a84df98ac002c0563b4

build/soong/android/config.go
@@ -1,0 +1,12 @@
+package android
+
+// Global config used by Lineage soong additions
+var LineageConfig = struct {
+	JavaSourcesOverlayModuleWhitelist []string
+}{
+	[]string{
+		// List of packages that are permitted
+		// for java sources overlay.
+		"org.lineageos.hardware",
+	},
+}

AfterlifeOS/vendor_afterlife

build/soong/android/config.go
@@ -1,0 +1,13 @@
+package android
+
+// Global config used by Lineage soong additions
+var LineageConfig = struct {
+	// List of packages that are permitted
+	// for java source overlays.
+	JavaSourceOverlayModuleWhitelist []string
+}{
+	// JavaSourceOverlayModuleWhitelist
+	[]string{
+		"org.lineageos.hardware",
+	},
+}