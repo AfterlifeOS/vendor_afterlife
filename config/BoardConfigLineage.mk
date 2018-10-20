LineageOS/android_vendor_lineage

lineage: Dynamically generate kernel headers using lineage generator
Add a soong vendor plugin for kernel config variables so we can use
these in go.

Change-Id: Id31f2be8fcc5aba2d965dbe815edaaf1d28279c6

config/BoardConfigLineage.mk
@@ -3,6 +3,8 @@ ifeq ($(WITH_LINEAGE_CHARGER),true)
     BOARD_HAL_STATIC_LIBRARIES := libhealthd.lineage
 endif
 
+include vendor/lineage/config/BoardConfigKernel.mk
+
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/lineage/config/BoardConfigQcom.mk
 endif

AfterlifeOS/vendor_afterlife

config/BoardConfigLineage.mk
@@ -8,3 +8,5 @@ include vendor/lineage/config/BoardConfigKernel.mk
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/lineage/config/BoardConfigQcom.mk
 endif
+
+include vendor/lineage/config/BoardConfigSoong.mk