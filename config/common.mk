7c4773d75378dca37048c5cd99ce4192ca4ac66b
Danny Lin <danny@kdrag0n.dev>
2020-11-06 21:56:45 -0800

    config: Disable RescueParty
    RescueParty's aggressive approach to error recovery almost never helps
    with the problems we're likely to face, and its risk of data loss is far
    too high. Force-disable it entirely and remove all other conditions,
    especially those that depend on data integrity in the first place, to
    reduce the risk of RescueParty triggering falsely and causing data loss.
    
    Note that it is still possible to force-enable RescueParty for testing
    purposes using the persist.sys.enable_rescue property.
    
    Change-Id: I70b36162889c698744af797df6fd7fd3ccd5df07
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -82,6 +82,10 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig
 
+# Disable RescueParty due to high risk of data loss
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.disable_rescue=true
+    
 # Extra tools in Afterlife
 PRODUCT_PACKAGES += \
     bash \
