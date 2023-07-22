DroidX-UI/vendor_droidx

vendor: use prebuilt bootanimation from us

build/tasks/bacon.mk
@@ -15,8 +15,10 @@
 
 # -----------------------------------------------------------------
 # Lineage OTA update package
-
-DROIDX_TARGET_PACKAGE := $(PRODUCT_OUT)/droidx-$(DROIDX_VERSION).zip
+ifeq ($(DROIDX_ZIP_TYPE), Gapps)
+	DROIDX_TARGET_PACKAGE := $(PRODUCT_OUT)/droidx-$(DROIDX_VERSION)-Gapps.zip
+else 
+    
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 

config/bootanimation.mk
@@ -1,0 +1,15 @@
+# Boot Animation
+
+# 1440p
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+PRODUCT_COPY_FILES += vendor/droidx/bootanimation/1440.zip:system/media/bootanimation.zip
+# 1080p
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+PRODUCT_COPY_FILES += vendor/droidx//bootanimation/1080.zip:system/media/bootanimation.zip
+# 720p
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),480)
+PRODUCT_COPY_FILES += vendor/droidx/bootanimation/720.zip:system/media/bootanimation.zip
+# Default to 1080p if the device does not set the flag.
+else
+PRODUCT_COPY_FILES += vendor/droidx/bootanimation/1080.zip:system/media/bootanimation.zip
+endif

config/common.mk
@@ -94,12 +94,6 @@ ifneq ($(TARGET_DISABLE_EPPE),true)
 $(call enforce-product-packages-exist-internal,$(wildcard device/*/$(DROIDX_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild Calendar Launcher3 Launcher3Go Launcher3QuickStep Launcher3QuickStepGo android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
 endif
 
-# Bootanimation
-TARGET_SCREEN_WIDTH ?= 1080
-TARGET_SCREEN_HEIGHT ?= 1920
-PRODUCT_PACKAGES += \
-    bootanimation.zip
-
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig
@@ -190,3 +184,4 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
     vendor/droidx/build/target/product/security/droidx
 
 include vendor/droidx/config/version.mk
+include vendor/droidx/config/bootanimation.mk

AfterlifeOS/vendor_afterlife

Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

build/tasks/bacon.mk
@@ -15,8 +15,10 @@
 
 # -----------------------------------------------------------------
 # Lineage OTA update package
+ifeq ($(AFTERLIFE_ZIP_TYPE), Gapps)
+	AFTERLIFE_TARGET_PACKAGE := $(PRODUCT_OUT)/afterlife-$(AFTERLIFE_VERSION)-Gapps.zip
+else 
 
-AFTERLIFE_TARGET_PACKAGE := $(PRODUCT_OUT)/afterlife-$(AFTERLIFE_VERSION).zip
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
 .PHONY: afterlife

config/bootanimation.mk
@@ -1,0 +1,15 @@
+# Boot Animation
+
+# 1440p
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+PRODUCT_COPY_FILES += vendor/afterlife/bootanimation/1440.zip:system/media/bootanimation.zip
+# 1080p
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+PRODUCT_COPY_FILES += vendor/afterlife//bootanimation/1080.zip:system/media/bootanimation.zip
+# 720p
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),480)
+PRODUCT_COPY_FILES += vendor/afterlife/bootanimation/720.zip:system/media/bootanimation.zip
+# Default to 1080p if the device does not set the flag.
+else
+PRODUCT_COPY_FILES += vendor/afterlife/bootanimation/1080.zip:system/media/bootanimation.zip
+endif

config/common.mk
@@ -5,8 +5,6 @@ PRODUCT_BRAND ?= AfterLife
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
-DROIDX_ZIP_TYPE := Vanilla
-
 # Gapps
 ifeq ($(AFTERLIFE_GAPPS), true)
     $(call inherit-product, vendor/gms/gms_full.mk)
@@ -93,12 +91,6 @@ ifneq ($(TARGET_DISABLE_EPPE),true)
 # Require all requested packages to exist
 $(call enforce-product-packages-exist-internal,$(wildcard device/*/$(AFTERLIFE_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild Calendar Launcher3 Launcher3Go Launcher3QuickStep Launcher3QuickStepGo android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
 endif
-
-# Bootanimation
-TARGET_SCREEN_WIDTH ?= 1080
-TARGET_SCREEN_HEIGHT ?= 1920
-PRODUCT_PACKAGES += \
-    bootanimation.zip
 
 # Config
 PRODUCT_PACKAGES += \
@@ -190,3 +182,4 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
     vendor/afterlife/build/target/product/security/afterlife
 
 include vendor/afterlife/config/version.mk
+include vendor/afterlife/config/bootanimation.mk