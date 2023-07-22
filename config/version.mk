DroidX-UI/vendor_droidx

vendor: Add official check and inital release maintainers

config/version.mk
@@ -17,10 +17,15 @@ ifndef DROIDX_BUILDTYPE
     endif
 endif
 
-# Filter out random types, so it'll reset to UNOFFICIAL
-ifeq ($(filter OFFICIAL,$(DROIDX_BUILDTYPE)),)
-    DROIDX_BUILDTYPE := UNOFFICIAL
-    DROIDX_EXTRAVERSION :=
+
+# Check Official
+ifeq ($(DROIDX_BUILD_TYPE), OFFICIAL)
+  LIST = $(shell cat vendor/droidx/droidx.devices)
+    ifeq ($(filter $(DROIDX_BUILD), $(LIST)), $(DROIDX_BUILD))
+      DROIDX_BUILD_TYPE := OFFICIAL
+    else 
+      DROIDX_BUILD_TYPE := UNOFFICIAL
+    endif
 endif
 
 ifeq ($(DROIDX_BUILDTYPE), UNOFFICIAL)

AfterlifeOS/vendor_afterlife

Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

config/version.mk
@@ -17,10 +17,14 @@ ifndef AFTERLIFE_BUILDTYPE
     endif
 endif
 
-# Filter out random types, so it'll reset to UNOFFICIAL
-ifeq ($(filter OFFICIAL,$(AFTERLIFE_BUILDTYPE)),)
-    AFTERLIFE_BUILDTYPE := UNOFFICIAL
-    AFTERLIFE_EXTRAVERSION :=
+# Check Official
+ifeq ($(AFTERLIFE_BUILD_TYPE), OFFICIAL)
+  LIST = $(shell cat vendor/afterlife/afterlife.devices)
+    ifeq ($(filter $(AFTERLIFE_BUILD), $(LIST)), $(AFTERLIFE_BUILD))
+      AFTERLIFE_BUILD_TYPE := OFFICIAL
+    else 
+      AFTERLIFE_BUILD_TYPE := UNOFFICIAL
+    endif
 endif
 
 ifeq ($(AFTERLIFE_BUILDTYPE), UNOFFICIAL)