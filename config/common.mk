911e766145b79812a47c4ff595d09b7d7e6a3683
Mr_ZerO <rbesar210311@gmail.com>
2023-10-17 13:04:49 +0000

    Version: Bump Version To Three Point O
    Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -9,7 +9,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 AFTERLIFE_ZIP_TYPE := Vanilla
 ifeq ($(AFTERLIFE_GAPPS), true)
     $(call inherit-product, vendor/gms/products/gms.mk)
-ifeq ($(AFTERLIFE_CORE),true)
+ifeq ($(AFTERLIFE_CORE), true)
     AFTERLIFE_ZIP_TYPE := CoreGApps
 else
     AFTERLIFE_ZIP_TYPE := Gapps
