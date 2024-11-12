a248e7597325f903cf592d0329dbf63e067e3eb5
Marko Man <darkobas@gmail.com>
2024-11-13 01:50:31 +0530

    vendor: Add black theme for dark mode [3/3] Change-Id: I6bfa5a8d1a1ae31444acbbf60da942c4961d2171 Signed-off-by: Jesse Chan <jc@lineageos.org>
    Change-Id: I30999f48a2000328dce591ed55a5cd0deaa7e716
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -199,6 +199,7 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/afterlife/overlay/common
 
 PRODUCT_PACKAGES += \
+    AndroidBlackThemeOverlay \
     CustomFontPixelLauncherOverlay \
     DocumentsUIOverlay \
     NavigationBarNoHintOverlay \
