4f2feaee5b41944a659c510403adf6dc65a2b8ee
Michael Bestas <mkbestas@lineageos.org>
2025-03-26 11:40:16 +0200

    soong: drop librmnetctl config
    Flag hasn't been ported since lineage-19.1.
    
    Change-Id: I2078d19cc80e4b64b86e2aab0bd68d7ee0f14224
    Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -66,8 +66,6 @@ SOONG_CONFIG_afterlifeNvidiaVars += \
     uses_nvidia_enhancements
 
 SOONG_CONFIG_NAMESPACES += afterlifeQcomVars
-SOONG_CONFIG_afterlifeQcomVars += \
-    uses_pre_uplink_features_netmgrd
 
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
@@ -82,7 +80,6 @@ SOONG_CONFIG_afterlifeGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GR
 SOONG_CONFIG_afterlifeGlobalVars_gralloc_handle_has_ubwcp_format := $(TARGET_GRALLOC_HANDLE_HAS_UBWCP_FORMAT)
 SOONG_CONFIG_afterlifeGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_afterlifeNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
-SOONG_CONFIG_afterlifeQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_afterlifeGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_afterlifeGlobalVars_camera_needs_client_info_lib_oplus := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB_OPLUS))
 
