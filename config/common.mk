LeafOS-Project/android_vendor_leaf

vendor: Introduce App Lock [4/4]
Change-Id: Ie6d86e301efe14146c961b11bfccd3fa74a1562c

config/common.mk
@@ -78,6 +78,10 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/leaf/overlay/common \
     vendor/leaf/overlay/no-rro
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    vendor/leaf/config/permissions/privapp-permissions-settings.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-settings.xml
+
 # PDF
 PRODUCT_PACKAGES += \
     MuPDF

AfterlifeOS/vendor_afterlife

Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>

config/common.mk
@@ -173,7 +173,8 @@ PRODUCT_PACKAGES += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    vendor/afterlife/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+    vendor/afterlife/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml \
+    vendor/afterlife/config/permissions/privapp-permissions-settings.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-settings.xml
 
 # Lineage Health
 PRODUCT_COPY_FILES += \