The-Clover-Project/vendor_clover

vendor: config: Copy Lineage health permissions Change-Id: I12dcb124293cac0ba6f7275ee30498b711c723dd

config/common.mk
@@ -184,6 +184,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/clover/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
 
+# Lineage Health
+PRODUCT_COPY_FILES += \
+    vendor/clover/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml
+
 # rsync
 PRODUCT_PACKAGES += \
     rsync

AfterlifeOS/vendor_afterlife

Change-Id: I071e85a74d32cdf491580294cdb1565e8c932aa1
Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>

config/common.mk
@@ -175,6 +175,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/afterlife/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
 
+# Lineage Health
+PRODUCT_COPY_FILES += \
+    vendor/afterlife/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml
+
 # rsync
 PRODUCT_PACKAGES += \
     rsync
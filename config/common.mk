669e13db7f78f8bd5bb9ec9a39d681984b0cc82c
Luofan Chen <clfbbn@gmail.com>
2025-02-01 02:03:58 +0530

    vendor: config: Copy Lineage health permissions Change-Id: I12dcb124293cac0ba6f7275ee30498b711c723dd
    Change-Id: I071e85a74d32cdf491580294cdb1565e8c932aa1
    Signed-off-by: Mr- 0 <oldcrocs21@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -175,6 +175,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/afterlife/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
 
+# Lineage Health
+PRODUCT_COPY_FILES += \
+    vendor/afterlife/config/permissions/org.lineageos.health.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/org.lineageos.health.xml
+
 # rsync
 PRODUCT_PACKAGES += \
     rsync
