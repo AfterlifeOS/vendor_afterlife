LineageOS/android_vendor_lineage

config: Use add_soong_config_namespace/add_soong_config_var macros
Test: m nothing and compare out/soong/soong.lineage_*.variables
Change-Id: I802f4f7eb37d520fd74599e2b673a5b2368c5d09

config/BoardConfigSoong.mk
@@ -15,16 +15,8 @@ EXPORT_TO_SOONG := \
 # Documentation here:
 # https://github.com/LineageOS/android_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
 
-SOONG_CONFIG_NAMESPACES += lineageVarsPlugin
-
-SOONG_CONFIG_lineageVarsPlugin :=
-
-define addVar
-  SOONG_CONFIG_lineageVarsPlugin += $(1)
-  SOONG_CONFIG_lineageVarsPlugin_$(1) := $($1)
-endef
-
-$(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
+$(call add_soong_config_namespace,lineageVarsPlugin)
+$(foreach v,$(EXPORT_TO_SOONG),$(eval $(call add_soong_config_var,lineageVarsPlugin,$(v))))
 
 # Camera
 ifneq ($(TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED),)

AfterlifeOS/vendor_afterlife

Test: m nothing and compare out/soong/soong.clover_*.variables
Signed-off-by: Mr_ZerO <rbesar210311@gmail.com>

config/BoardConfigSoong.mk
@@ -15,16 +15,8 @@ EXPORT_TO_SOONG := \
 # Documentation here:
 # https://github.com/LineageOS/android_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
 
-SOONG_CONFIG_NAMESPACES += afterlifeVarsPlugin
-
-SOONG_CONFIG_afterlifeVarsPlugin :=
-
-define addVar
-  SOONG_CONFIG_afterlifeVarsPlugin += $(1)
-  SOONG_CONFIG_afterlifeVarsPlugin_$(1) := $($1)
-endef
-
-$(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
+$(call add_soong_config_namespace,afterlifeVarsPlugin)
+$(foreach v,$(EXPORT_TO_SOONG),$(eval $(call add_soong_config_var,afterlifeVarsPlugin,$(v))))
 
 SOONG_CONFIG_NAMESPACES += afterlifeGlobalVars
 SOONG_CONFIG_afterlifeGlobalVars += \