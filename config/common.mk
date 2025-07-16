407c4c0b187c1635402c0c936bce3613941acc72
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-16 16:11:25 +0000

    vendor: Add blur support
    Change-Id: I1ced2b3c13c723886aaf46764ba294c18d6ff376

--- config/common.mk
+++ config/common.mk
@@ -62,6 +62,18 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/afterlife/prebuilt/common/etc/init/init.afterlife-system_ext.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.afterlife-system_ext.rc
 
+# Blur effect
+TARGET_ENABLE_BLUR ?= false
+ifeq ($(TARGET_ENABLE_BLUR),true)
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=true
+else
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.custom.blur.enable=false
+endif
+
+PRODUCT_SYSTEM_PROPERTIES += ro.surface_flinger.supports_background_blur=1
+
 # BtHelper
 PRODUCT_PACKAGES += \
     BtHelper
