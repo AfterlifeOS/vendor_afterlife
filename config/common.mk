0e9c83b5bdd4f7243a2639cbd741ebff039bc4ca
Aaron Kling <webgeek1234@gmail.com>
2025-07-21 00:32:21 -0500

    common: Allow vendors to exclude bp paths
    This resolves a couple issues. Allowing bp's that don't resolve to be
    in-tree. And allowing multiple copies of the same target.
    
    Change-Id: I85cfc566e18ea9fe1cb4cd8d70a71ef80517fd41

--- config/common.mk
+++ config/common.mk
@@ -10,6 +10,9 @@ $(call inherit-product, vendor/afterlife/config/Afterlife_bootanimation.mk)
 # Certification
 $(call inherit-product-if-exists, vendor/certification/config.mk)
 
+# Allow vendor prebuilt repos to exclude themselves from bp scanning
+-include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
+
 PRODUCT_BRAND ?= AfterlifeOS
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
