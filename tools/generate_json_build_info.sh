49de844d6eb1fa81be570417bb1b3a46bf0cbe4b
HinohArata <mmgcntk@gmail.com>
2026-01-01 14:17:39 +0700

    tools: Refactor ota JSON generator
    Signed-off-by: HinohArata <mmgcntk@gmail.com>

--- tools/generate_json_build_info.sh
+++ tools/generate_json_build_info.sh
@@ -1,111 +1,113 @@
 #!/bin/bash
 #
 # Copyright (C) 2019-2023 crDroid Android Project
+# Copyright (C) 2024 AfterlifeOS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
 
 # $1=TARGET_DEVICE, $2=PRODUCT_OUT, $3=FILE_NAME
-existingOTAjson=./device/afterlife/ota/$1/updates.json
-output=$2/$1.json
+TARGET_DEVICE=$1
+PRODUCT_OUT=$2
+FILE_NAME=$3
 
-# cleanup old file
-if [ -f $output ]; then
-        rm $output
-fi
+# Input: Existing Master JSON in Source Tree (Optional, for merging)
+OTA_JSON_PATH="./device/afterlife/ota/$TARGET_DEVICE/updates.json"
+# Output: Result JSON in Product Out (For Jenkins artifact/upload)
+OUTPUT_JSON_PATH="$PRODUCT_OUT/$TARGET_DEVICE.json"
+
+PYTHON_GENERATOR="./vendor/afterlife/tools/ota_json_generator.py"
 
 echo "Generating JSON file data for OTA support..."
 
-if [ -f $existingOTAjson ]; then
-        # get data from already existing device json
-        maintainer=`grep -n "\"maintainer\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
-        oem=`grep -n "\"oem\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
-        device=`grep -n "\"device\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
-        filename=$3
-        version=`echo "$3" | cut -d '-' -f 2 | cut -d 'V' -f 2`
-        codename=`echo "$3" | cut -d '-' -f 3`
-        buildprop=$2/system/build.prop
-        linenr=`grep -n "ro.system.build.date.utc" $buildprop | cut -d':' -f1`
-        timestamp=`sed -n $linenr'p' < $buildprop | cut -d'=' -f2`
-        md5=`md5sum "$2/$3" | cut -d' ' -f1`
-        sha256=`sha256sum "$2/$3" | cut -d' ' -f1`
-        size=`stat -c "%s" "$2/$3"`
-        buildtype=`grep -n "\"buildtype\"" $existingOTAjson | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs`
-        forum=`grep -n "\"forum\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
-        if [ ! -z "$forum" ]; then
-                forum="https:"$forum
-        fi
-        telegram=`grep -n "\"telegram\"" $existingOTAjson | cut -d ":" -f 4 | sed 's/"//g' | sed 's/,//g' | xargs`
-        if [ ! -z "$telegram" ]; then
-                telegram="https:"$telegram
-        fi
+# Extract Build Info
+VERSION=$(echo "$FILE_NAME" | cut -d '-' -f 2 | cut -d 'V' -f 2)
+# Parsing variant: Assuming format ...-date-variant.zip
+VARIANT=$(echo "$FILE_NAME" | sed 's/.zip//' | awk -F'-' '{print $NF}')
 
-
-        echo '{
-    "response": [
-        {
-            "maintainer": "'$maintainer'",
-            "oem": "'$oem'",
-            "device": "'$device'",
-            "version": "'$version'",
-            "codename": "'$codename'",
-            "filename": "'$filename'",
-            "download": "https://afterlifeos.com/device/'$1'",
-            "timestamp": '$timestamp',
-            "md5": "'$md5'",
-            "sha256": "'$sha256'",
-            "size": '$size',
-            "buildtype": "'$buildtype'",
-            "forum": "'$forum'",
-            "telegram": "'$telegram'"
-        }
-    ]
-}' >> $output
-
-else
-        filename=$3
-        version=`echo "$3" | cut -d '-' -f 2 | cut -d 'V' -f 2`
-        codename=`echo "$3" | cut -d '-' -f 3`
-        buildprop=$2/system/build.prop
-        linenr=`grep -n "ro.system.build.date.utc" $buildprop | cut -d':' -f1`
-        timestamp=`sed -n $linenr'p' < $buildprop | cut -d'=' -f2`
-        md5=`md5sum "$2/$3" | cut -d' ' -f1`
-        sha256=`sha256sum "$2/$3" | cut -d' ' -f1`
-        size=`stat -c "%s" "$2/$3"`
-
-        echo '{
-    "response": [
-        {
-            "maintainer": "''",
-            "oem": "''",
-            "device": "''",
-            "version": "'$version'",
-            "codename": "'$codename'",
-            "filename": "'$filename'",
-            "download": "https://sourceforge.net/projects/afterlife-projects/files/release/'$1'/'$3'/download",
-            "timestamp": '$timestamp',
-            "md5": "'$md5'",
-            "sha256": "'$sha256'",
-            "size": '$size',
-            "buildtype": "''",
-            "forum": "''",
-            "telegram": "''"
-        }
-    ]
-}' >> $output
-
-        echo 'There is no official support for this device yet'
-        echo 'Apply from official from https://t.me/Afterlife_update'
+# Fallback if variant detection fails or is empty
+if [[ "$FILE_NAME" == *"gapps"* ]]; then
+    if [[ "$FILE_NAME" == *"coregapps"* ]]; then
+        VARIANT="coregapps"
+    elif [[ "$FILE_NAME" == *"basicgapps"* ]]; then
+        VARIANT="basicgapps"
+    else
+        VARIANT="gapps"
+    fi
+elif [[ "$FILE_NAME" == *"vanilla"* ]]; then
+    VARIANT="vanilla"
 fi
 
-echo ""
+BUILD_PROP="$PRODUCT_OUT/system/build.prop"
+
+# Get Timestamp (Simplified)
+TIMESTAMP=$(grep "^ro.system.build.date.utc=" "$BUILD_PROP" | cut -d'=' -f2)
+
+# Get Maintainer from Build Prop (Priority 1 - Simplified)
+MAINTAINER=$(grep "^ro.afterlife.maintainer=" "$BUILD_PROP" | cut -d'=' -f2)
+
+# Calculate Checksums and Size
+MD5=$(md5sum "$PRODUCT_OUT/$FILE_NAME" | cut -d' ' -f1)
+SHA256=$(sha256sum "$PRODUCT_OUT/$FILE_NAME" | cut -d' ' -f1)
+SIZE=$(stat -c "%s" "$PRODUCT_OUT/$FILE_NAME")
+
+# Meta Info
+OEM=""
+FORUM=""
+TELEGRAM=""
+
+# Priority 2: Grep from existing JSON (if available & maintainer still empty)
+if [ -f "$OTA_JSON_PATH" ]; then
+    echo "  Input JSON: $OTA_JSON_PATH (Found)" 
+    
+    if [ -z "$MAINTAINER" ]; then
+        MAINTAINER=$(grep -n "\"maintainer\"" $OTA_JSON_PATH | head -n 1 | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs)
+    fi
+    
+    OEM=$(grep -n "\"oem\"" $OTA_JSON_PATH | head -n 1 | cut -d ":" -f 3 | sed 's/"//g' | sed 's/,//g' | xargs)
+    FORUM=$(grep -n "\"forum\"" $OTA_JSON_PATH | head -n 1 | cut -d ":" -f 4- | sed 's/"//g' | sed 's/,//g' | xargs)
+    TELEGRAM=$(grep -n "\"telegram\"" $OTA_JSON_PATH | head -n 1 | cut -d ":" -f 4- | sed 's/"//g' | sed 's/,//g' | xargs)
+else
+    echo "  Input JSON: $OTA_JSON_PATH (Not Found - Will Create New)"
+fi
+
+# Priority 3: Default Fallbacks
+if [ -z "$MAINTAINER" ]; then MAINTAINER="Unknown Maintainer"; fi
+if [ -z "$OEM" ]; then OEM="AfterlifeOS"; fi
+if [ -z "$FORUM" ]; then FORUM="https://t.me/yaseaprjktchat"; fi
+if [ -z "$TELEGRAM" ]; then TELEGRAM="https://t.me/Afterlife_update"; fi
+
+# Fix URL parsing
+if [[ $FORUM != http* ]]; then FORUM="https:$FORUM"; fi
+if [[ $TELEGRAM != http* ]]; then TELEGRAM="https:$TELEGRAM"; fi
+
+UNIFIED_URL="https://afterlifeos.com/device/$TARGET_DEVICE"
+
+echo "  Device: $TARGET_DEVICE"
+echo "  Variant: $VARIANT"
+echo "  Version: $VERSION"
+echo "  Maintainer: $MAINTAINER"
+echo "  Output JSON: $OUTPUT_JSON_PATH"
+
+# Call Python Generator
+python3 "$PYTHON_GENERATOR" \
+    --json_path "$OTA_JSON_PATH" \
+    --output_path "$OUTPUT_JSON_PATH" \
+    --device "$TARGET_DEVICE" \
+    --maintainer "$MAINTAINER" \
+    --oem "$OEM" \
+    --version "$VERSION" \
+    --variant "$VARIANT" \
+    --filename "$FILE_NAME" \
+    --download_url "$UNIFIED_URL" \
+    --timestamp "$TIMESTAMP" \
+    --md5 "$MD5" \
+    --sha256 "$SHA256" \
+    --size "$SIZE" \
+    --forum "$FORUM" \
+    --telegram "$TELEGRAM"
+
+echo "Done."
