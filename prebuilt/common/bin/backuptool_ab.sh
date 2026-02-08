DerpFest-AOSP/android_vendor_derpfest

backuptool: Restore /system/build.prop checks
Required for 23.0/23.1 -> 23.2 transition.

Change-Id: I608743f11eb29917f109dd490624b0126c9fbb88

prebuilt/common/bin/backuptool_ab.sh
@@ -49,14 +49,14 @@ restore_addon_d() {
   fi
 }
 
-# Proceed only if /product is the expected major and minor version
+# Proceed only if /product or /system is the expected major and minor version
 check_prereq() {
 # If there is no build.prop file the partition is probably empty.
-if [ ! -r /product/etc/build.prop ]; then
+if [ ! -r /product/etc/build.prop ] && [ ! -r /system/build.prop ]; then
   echo "Backup/restore is not possible. Partition is probably empty"
   return 1
 fi
-if ! grep -q "^ro.lineage.version=$V.*" /product/etc/build.prop; then
+if ! grep -q "^ro.lineage.version=$V.*" /product/etc/build.prop /system/build.prop; then
   echo "Backup/restore is not possible. Incompatible ROM version: $V"
   return 2
 fi

AfterlifeOS/vendor_afterlife

prebuilt/common/bin/backuptool_ab.sh
@@ -49,14 +49,14 @@ restore_addon_d() {
   fi
 }
 
-# Proceed only if /product is the expected major and minor version
+# Proceed only if /product or /system is the expected major and minor version
 check_prereq() {
 # If there is no build.prop file the partition is probably empty.
-if [ ! -r /product/etc/build.prop ]; then
+if [ ! -r /product/etc/build.prop ] && [ ! -r /system/build.prop ]; then
   echo "Backup/restore is not possible. Partition is probably empty"
   return 1
 fi
-if ! grep -q "^ro.afterlife.version=$V.*" /product/etc/build.prop; then
+if ! grep -q "^ro.afterlife.version=$V.*" /product/etc/build.prop /system/build.prop; then
   echo "Backup/restore is not possible. Incompatible ROM version: $V"
   return 2
 fi