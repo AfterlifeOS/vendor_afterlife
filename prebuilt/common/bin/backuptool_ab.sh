AxionAOSP/android_vendor_lineage

backuptool: Update build.prop file path
See: If4e77019427793b5c1f3dec9fc938ac3e3270c26
Change-Id: I4d7d89fba8cbe3cc3f154c7230a8c3a386c8cfd1

prebuilt/common/bin/backuptool_ab.sh
@@ -49,14 +49,14 @@ restore_addon_d() {
   fi
 }
 
-# Proceed only if /system is the expected major and minor version
+# Proceed only if /product is the expected major and minor version
 check_prereq() {
 # If there is no build.prop file the partition is probably empty.
-if [ ! -r /system/build.prop ]; then
+if [ ! -r /product/etc/build.prop ]; then
   echo "Backup/restore is not possible. Partition is probably empty"
   return 1
 fi
-if ! grep -q "^ro.lineage.version=$V.*" /system/build.prop; then
+if ! grep -q "^ro.lineage.version=$V.*" /product/etc/build.prop; then
   echo "Backup/restore is not possible. Incompatible ROM version: $V"
   return 2
 fi

AfterlifeOS/vendor_afterlife

prebuilt/common/bin/backuptool_ab.sh
@@ -49,14 +49,14 @@ restore_addon_d() {
   fi
 }
 
-# Proceed only if /system is the expected major and minor version
+# Proceed only if /product is the expected major and minor version
 check_prereq() {
 # If there is no build.prop file the partition is probably empty.
-if [ ! -r /system/build.prop ]; then
+if [ ! -r /product/etc/build.prop ]; then
   echo "Backup/restore is not possible. Partition is probably empty"
   return 1
 fi
-if ! grep -q "^ro.afterlife.version=$V.*" /system/build.prop; then
+if ! grep -q "^ro.afterlife.version=$V.*" /product/etc/build.prop; then
   echo "Backup/restore is not possible. Incompatible ROM version: $V"
   return 2
 fi